3368276d40123a338ef95491cfed8874137dc3d2
MichaelKema <mkkema02@gmail.com>
2026-02-11 09:32:23 -0500

    added filler home and file pages

--- frontend/movies/app/pages/films/page.tsx
+++ frontend/movies/app/pages/films/page.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
       <header className="page-header">
         <h1 className="page-title">
-          <Link href="/pages/films">Movies</Link>
+          <Link href="/">Home</Link>
         </h1>
 
       </header>
     </div>
   );
 }
+
